8955707a42c418415c725b39617d2fddbef73728
Aki <sekai.110.0604@gmail.com>
2025-10-12 20:21:01 +0100

    chore: migrate styling from styled-components to Emotion and align with MUI v7

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,19 +1,59 @@
-import React, { Fragment, useEffect } from 'react';
-
-// apis
+import React, { Fragment, useReducer, useEffect } from 'react';
+import styled from '@emotion/styled';
 import { fetchRestaurants } from '../apis/restaurants';
+import {
+  initialState,
+  restaurantsActionTyps,
+  restaurantsReducer,
+} from '../reducers/restaurants';
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png';
+
+const HeaderWrapper = styled('div')`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled('img')`
+  height: 90px;
+`;
+
+const MainCoverImageWrapper = styled('div')`
+  text-align: center;
+`;
+
+const MainCover = styled('img')`
+  height: 600px;
+`;
 
 export const Restaurants = () => {
+  const [state, dispatch] = useReducer(restaurantsReducer, initialState);
 
   useEffect(() => {
-    fetchRestaurants()
-    .then((data) =>
-      console.log(data)
-    )
-  }, [])
+    dispatch({ type: restaurantsActionTyps.FETCHING });
+    fetchRestaurants().then((data) =>
+      dispatch({
+        type: restaurantsActionTyps.FETCH_SUCCESS,
+        payload: { restaurants: data?.restaurants ?? [] },
+      })
+    );
+  }, []);
+
   return (
     <Fragment>
-      レストラン一覧
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
+
+      {/* レストラン一覧 */}
+      {Array.isArray(state.restaurantsList) &&
+        state.restaurantsList.map((r) => (
+          <div key={r.id ?? r.name}>{r.name}</div>
+        ))}
     </Fragment>
-  )
-}
+  );
+};
